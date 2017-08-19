d682b15d5bc9ac5364fe0b101455d733a89d5a4e
rafaeel16 <rafaeel.16@gmail.com>
2017-08-19 00:54:40 -0300

    add Materialize Toast and Validation form

--- loc/js/app.js
+++ loc/js/app.js
@@ -1,27 +1,26 @@
  $(document).ready(function () {
 
-    //Seleciona o input para Cadastro do Herói na página de Cadastro
+     //Seleciona o input para Cadastro do Herói na página de Cadastro
      $("#nome-heroi").focus();
 
      //Ajax preencher o Select de Heróis
      $.ajax({
          url: "https://api-loc-rafaeel16.c9users.io/api/heroes",
          type: "GET",
-         dataType:"JSON",
+         dataType: "JSON",
          success: function (response) {
              console.log(response);
-            var optionHeroi = "<option value='0'>--Selecione um Herói--</option>";
-            $.each(response, function(i, item) {
-                optionHeroi += "<option value='" + item.id + "'>" + item.name + "</option>";
-            }); //Fim Each
+             var optionHeroi = "<option value='0'>--Selecione um Herói--</option>";
+             $.each(response, function (i, item) {
+                 optionHeroi += "<option value='" + item.id + "'>" + item.name + "</option>";
+             }); //Fim Each
 
-            $("#ddlHeroi").append(optionHeroi);
-            $("#ddlHeroi").material_select();
+             $("#ddlHeroi").append(optionHeroi);
+             $("#ddlHeroi").material_select();
 
          },
          error: function (e) {
              console.log("Erro: " + e);
-             alert("Erro: " + e);
          }
 
      });
@@ -30,20 +29,19 @@
          url: "https://api-loc-rafaeel16.c9users.io/api/positions",
          type: "GET",
          dataType: "JSON",
-        success: function(response){
-            console.log(response);
-            var optionPosition = "<option value='0'>--Selecione uma Posição--</option>";
-            $.each(response, function(i, item){
-                optionPosition += "<option value='" + item.id + "'>" + item.name + "</option>";
-            }); //Fim Each
+         success: function (response) {
+             console.log(response);
+             var optionPosition = "<option value='0'>--Selecione uma Posição--</option>";
+             $.each(response, function (i, item) {
+                 optionPosition += "<option value='" + item.id + "'>" + item.name + "</option>";
+             }); //Fim Each
 
-            $("#ddlPosition").append(optionPosition);
-            $("#ddlPosition").material_select();
-        },
-        error: function(e){
-            console.log("Erro: " + e);
-            alert("Erro: " + e);
-        }
+             $("#ddlPosition").append(optionPosition);
+             $("#ddlPosition").material_select();
+         },
+         error: function (e) {
+             console.log("Erro: " + e);
+         }
      });
 
 
@@ -54,25 +52,30 @@
  function CadastraHeroi() {
      var name = $("#nome-heroi").val();
      var icon = $("#nome-icone").val();
-     $.ajax({
-         type: "POST",
-         data: {
-             "name": name,
-             "icon": icon
-         },
-         url: "https://api-loc-rafaeel16.c9users.io/api/heroes",
-         success: function (data) {
-             console.log("Herói Cadastrado com sucesso.");
 
-             $("#nome-heroi").val("")
-             $("#nome-icone").val("");
+     if (name && icon != "") {
+         $.ajax({
+             type: "POST",
+             data: {
+                 "name": name,
+                 "icon": icon
+             },
+             url: "https://api-loc-rafaeel16.c9users.io/api/heroes",
+             success: function (data) {
+                 console.log("Herói Cadastrado com sucesso.");
 
-             window.location.reload();
-         },
-         error: function (e) {
-             alert("Erro: " + e);
-         }
-     });
+                 $("#nome-heroi").val("")
+                 $("#nome-icone").val("");
+
+                 window.location.reload();
+             },
+             error: function (e) {
+                 alert("Erro: " + e);
+             }
+         });
+     } else {
+        Materialize.toast("Preencha os dois Campos!",4000);
+     }
  }
 
  // Captura o ENTER para executar a função de Cadastrar o Herói
