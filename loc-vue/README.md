1b7b890ec735b7eac7950e6e4368ac5f1234afb4
rafaeel16 <rafaeel.16@gmail.com>
2018-02-17 00:13:15 -0200

    coding front + get calls in JS

--- loc-vue/README.md
+++ loc-vue/README.md
@@ -1,3 +1,7 @@
 Front do Sistema League of Counter
 
 Pesquisar alguns frameworks tipo o vuetify para construir o front. 
+
+Verificar se vou continuar com o Vuetify
+
+Regra 1: não utilizar Jquery.
--- loc-vue/js/app.js
+++ loc-vue/js/app.js
@@ -1,6 +1,47 @@
-new Vue({
+var app = new Vue({
   el:'#app',
+  
   data: {
+    listHeroes: {},
+    listPositions: {},
+
+  },
+
+  mounted(){
+    this.getHeroes(),
+    this.getPositions(),
+    this.getCounterHeroes()
+  },
+
+  methods: {
     
+    getHeroes: function () {
+      axios.get('https://api-loc-rafaeel16.c9users.io/api/heroes').then((response) => {
+        console.log(response)
+        this.listHeroes = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
+
+    getPositions: function () {
+      axios.get('https://api-loc-rafaeel16.c9users.io/api/positions').then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
+
+    getCounterHeroes: function () {
+      axios.get('https://api-loc-rafaeel16.c9users.io/api/countersheroes').then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+
   }
 })
