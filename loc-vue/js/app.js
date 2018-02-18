7e3a12513d74b97d828fba7eda5ec31db6668ec5
rafaeel16 <rafaeel.16@gmail.com>
2018-02-18 19:38:41 -0300

    ddlHero working

--- loc-vue/js/app.js
+++ loc-vue/js/app.js
@@ -1,47 +1,50 @@
 var app = new Vue({
-  el:'#app',
-  
+  el: '#app',
+
   data: {
     listHeroesOptions: {},
-    listPositionsOptions: {},
-
+    listPositionsOptions: {}
   },
 
-  mounted(){
+  mounted() {
     this.getHeroes(),
     this.getPositions(),
     this.getCounterHeroes()
   },
 
   methods: {
-    
+
     getHeroes: function () {
       axios.get('https://api-loc-rafaeel16.c9users.io/api/heroes').then((response) => {
-        console.log(response)
-        this.listHeroesOptions = response.data
-        $('#ddlHero').material_select()
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+          console.log(response)
+          this.listHeroesOptions = response.data
+
+          setTimeout(function () {
+            $('#ddlHero').material_select()
+          }, 0);
+
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     },
 
     getPositions: function () {
       axios.get('https://api-loc-rafaeel16.c9users.io/api/positions').then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     },
 
     getCounterHeroes: function () {
       axios.get('https://api-loc-rafaeel16.c9users.io/api/countersheroes').then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
 
   }
