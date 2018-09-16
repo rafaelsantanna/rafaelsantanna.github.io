c30b640fb5f1478cb1ff050d260441bff69e7365
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-09-16 14:06:04 -0300

    Estrutura HTML

---
+++ freela/avanutri/login/js/app.js
@@ -0,0 +1,23 @@
+/* ---------------------------------------------------
+    LOGIN FORM STYLE
+----------------------------------------------------- */
+
+var working = false;
+$('.login').on('submit', function (e) {
+    e.preventDefault();
+    if (working) return;
+    working = true;
+    var $this = $(this),
+        $state = $this.find('button > .state');
+    $this.addClass('loading');
+    $state.html('Autenticando');
+    setTimeout(function () {
+        $this.addClass('ok');
+        $state.html('Bem vindo de volta!');
+        setTimeout(function () {
+            $state.html('Log in');
+            $this.removeClass('ok loading');
+            working = false;
+        }, 4000);
+    }, 3000);
+});
--- freela/avanutri/readme.md
+++ freela/avanutri/readme.md
@@ -3,3 +3,6 @@ Referências:
 FAB:  
 https://codepen.io/cstamant/pen/vORzMm  
 https://codepen.io/noda/pen/ozyaKz  
+
+Login Form:  
+https://codepen.io/boudra/pen/YXzLBN  
