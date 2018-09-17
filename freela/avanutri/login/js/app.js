f317344d418621f743c60452bff6818a407b7128
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-09-16 23:21:25 -0300

    versão desktop

--- freela/avanutri/login/js/app.js
+++ freela/avanutri/login/js/app.js
@@ -11,12 +11,14 @@ $('.login').on('submit', function (e) {
         $state = $this.find('button > .state');
     $this.addClass('loading');
     $state.html('Autenticando');
+    $('.spinner').show();
     setTimeout(function () {
         $this.addClass('ok');
         $state.html('Bem vindo de volta!');
         setTimeout(function () {
             $state.html('Log in');
             $this.removeClass('ok loading');
+            $('.spinner').hide();
             working = false;
         }, 4000);
     }, 3000);
