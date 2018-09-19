c0374c15be0efd297d08b0e8fc52d1bb5b50ceaa
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-09-19 00:42:52 -0300

    implementando menu dropdown

--- freela/avanutri/interno/js/app.js
+++ freela/avanutri/interno/js/app.js
@@ -1,3 +1,21 @@
 $('.fab').click(function() {
   $(this).toggleClass('open');
 });
+
+var dropdown = document.querySelectorAll('.custom-dropdown');
+
+dropdown.forEach(function(item){
+  item.querySelector('a').addEventListener('click', function(event) {
+    event.preventDefault();
+    var parent = item.parentNode;
+    var child = parent.querySelector('.custom-dropdown-menu');
+    if(child.style.display == 'block') {
+      child.style.display = 'none';
+      child.classList.remove('open');
+    } else {
+      child.style.display = 'block';
+      child.classList.add('open');
+    }
+    
+  });
+});
