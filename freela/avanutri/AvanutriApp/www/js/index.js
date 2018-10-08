b5c074a5e7f9b0db51b8789b9b1bbefb432d9690
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-10-07 23:37:54 -0300

    tela lista de receitas e receita

--- freela/avanutri/AvanutriApp/www/js/index.js
+++ freela/avanutri/AvanutriApp/www/js/index.js
@@ -1,4 +1,10 @@
+// Initial all accordion
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     M.Collapsible.init(elems, 'accordion');
   });
+
+
+// INIT Tabs recipe details
+var tabs = document.querySelector('#tabs-recipe');
+M.Tabs.init(tabs);
