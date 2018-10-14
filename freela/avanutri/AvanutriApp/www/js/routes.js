725adae5ee7313c64d083566a3e2f7656626e648
Rafael Sant' Anna <rafaeel.16@gmail.com>
2018-10-13 22:59:07 -0300

    adiciona animação na transição

--- freela/avanutri/AvanutriApp/www/js/routes.js
+++ freela/avanutri/AvanutriApp/www/js/routes.js
@@ -8,40 +8,47 @@ function navigateTo(page, event, self) {
         event.preventDefault();
     }
 
-    limparTemplate();
-    $('#content').load('../pages/' + page + '.html');
+    // Animação na transição de tela
+    $('#content nav~div').fadeOut();
 
-    // Condição que verifica se o menu inferior está escondido, se sim mostre
-    if($('#navigation-bottom').css('display', 'none')){
-        $('#navigation-bottom').css('display', 'flex');
-    }
+    // Timeout para dar tempo para a animação
+    setTimeout(function () {
+        limparTemplate();
+        $('#content').load('../pages/' + page + '.html');
+
+        // Condição que verifica se o menu inferior está escondido, se sim mostre
+        if ($('#navigation-bottom').css('display', 'none')) {
+            $('#navigation-bottom').css('display', 'flex');
+        }
+
+        //Iniciando abas
+        if (page == 'recipe-details') {
+            setTimeout(function () {
+                $('.tabs').tabs();
+            }, 100);
+        }
+
+    }, 400);
 
     //Condição para esconder o menu inferior se for 'x' página
-    if(page == 'forgot-account' || page == 'login'){
+    if (page == 'forgot-account' || page == 'login') {
         $('#navigation-bottom').css('display', 'none');
     }
 
     // Iniciando accordion
-    if(page == 'dietary-prescription' || page == 'replacement-list') {
-        setTimeout(function() {
+    if (page == 'dietary-prescription' || page == 'replacement-list') {
+        setTimeout(function () {
             initAccordion();
-        },100);
-    }
-
-    //Iniciando abas
-    if(page == 'recipe-details') {
-        setTimeout(function() {
-            $('.tabs').tabs();
         }, 100);
     }
 
     // Fechando o sidemenu
-    if(page == 'notifications') {
+    if (page == 'notifications') {
         $('.sidenav').sidenav('close');
     }
 
     // Condição para ativar o primeiro icone do menu inferior após o login
-    if($(self).hasClass('btn-login-enter')) {
+    if ($(self).hasClass('btn-login-enter')) {
         var active = $('#navigation-bottom .navigation-item:first');
         activeMenuIcon(active);
     } else {
